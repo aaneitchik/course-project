05e539252e81e304238d6a87aefcb5d107ccd586
aaneitchik <aaneitchik@gmail.com>
2015-12-15 14:27:56 +0300

    Add logout possibility

--- client/app/components/navbar/navbar.directive.js
+++ client/app/components/navbar/navbar.directive.js
@@ -15,10 +15,23 @@
         function navbar() {
             var directive = {
                 restrict: 'E',
+                controller: NavbarController,
+                controllerAs: 'navbar',
                 templateUrl: 'app/components/navbar/navbar.html'
             };
 
             return directive;
+        }
+
+        /*@ngInject*/
+        function NavbarController($scope, EVENTS) {
+            var vm = this;
+            vm.currentUser = $scope.currentUser;
+            $scope.$on(EVENTS.userChanged, function() {
+                console.log('changed user');
+                console.log($scope.currentUser);
+                vm.currentUser = $scope.currentUser;
+            });
         }
 
     })();
